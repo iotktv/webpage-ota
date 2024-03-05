399a44afbf45f610b87c18d191a0f2c907668b67
iotktv <wahyudi.prasatia@koltiva.com>
2024-03-05 11:21:02 +0700

    initcommmit

---
+++ precache-manifest.0ba675b8d2e7a732efe67837efed7f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1beb7f7afca2c227e80d8e04b118ed44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b2c30182ba6e110fda6",
+    "url": "/static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "63434a638cabb531d86c",
+    "url": "/static/js/2.ddd56c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddd56c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2c30182ba6e110fda6",
+    "url": "/static/js/main.9dd50d33.chunk.js"
+  },
+  {
+    "revision": "dab82071668f2702c64d",
+    "url": "/static/js/runtime-main.ab6486c8.js"
+  }
+]);
